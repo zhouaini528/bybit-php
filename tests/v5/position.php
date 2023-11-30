45f98aac87968c89a6479ff71c2e38957b102289
zhouaini528 <465382251@qq.com>
2023-11-30 16:36:23 +0800

    add tests v5

---
+++ tests/v5/position.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @author lin <465382251@qq.com>
+ * */
+use \Lin\Bybit\BybitV5;
+
+require __DIR__ .'../../../vendor/autoload.php';
+
+include 'key_secret.php';
+
+$bybit=new BybitV5($key,$secret);
+
+//You can set special needs
+$bybit->setOptions([
+    //Set the request timeout to 60 seconds by default
+    'timeout'=>10,
+
+    'headers'=>[
+        //X-Referer or Referer - 經紀商用戶專用的頭參數
+        //X-BAPI-RECV-WINDOW 默認值為5000
+        //cdn-request-id
+        'X-BAPI-RECV-WINDOW'=>'6000',
+    ]
+]);
+
+
+try {
+    $result=$bybit->position()->getList([
+        'category'=>'linear',
+        'symbol'=>'BTCUSDT',
+    ]);
+    print_r($result);
+}catch (\Exception $e){
+    print_r($e->getMessage());
+}
